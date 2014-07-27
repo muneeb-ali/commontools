a2f01826c9b3a41fbb1d5306c15c7a558a6a82cc
Muneeb Ali <muneeb@ali.vc>
2014-07-26 22:23:22 -0700

    some common functions that are generally useful

---
+++ common/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+	python-common
+	~~~~~
+
+	:copyright: (c) 2014 by Muneeb Ali <http://muneebali.com>
+	:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1.0'
+
+from .common import setup_logging
+from .common import pretty_dump, pretty_print
+from .common import utf8len
+from .common import get_json, get_string
+from .common import error_reply
---
+++ common/common.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    python-common
+    ~~~~~
+
+    :copyright: (c) 2014 by Muneeb Ali <http://muneebali.com>
+    :license: MIT, see LICENSE for more details.
+"""
+
+import os 
+import sys
+import json
+import logging.config
+
+#-------------------------
+def setup_logging(
+    file_name='logging.json', 
+    default_level=logging.INFO,
+):
+    """
+        Setup logging configuration
+    """
+
+    current_dir =  os.path.abspath(os.path.dirname(__file__))
+    
+    print current_dir
+    file_path = current_dir + '/' + file_name 
+
+    if os.path.exists(file_path):
+        with open(file_path, 'rt') as f:
+            data = f.read()
+            config = json.loads(data)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+#-------------------------
+def pretty_dump(input):
+
+    return json.dumps(input, cls=MongoEncoder, sort_keys=False, indent=4, separators=(',', ': '))
+
+#-------------------------
+def pretty_print(input):
+    log.debug(pretty_dump(input))
+
+#-----------------------------------
+def utf8len(s):
+
+    if type(s) == unicode:
+        return len(s)
+    else:
+        return len(s.encode('utf-8'))
+
+#-----------------------------------------
+def get_json(data):
+
+    if isinstance(data,dict):
+        pass 
+    else:
+        data = json.loads(data)
+        
+    return data
+
+#-----------------------------------------
+def get_string(data):
+
+    if isinstance(data,dict):
+        data = json.dumps(data) 
+    else:
+        pass
+        
+    return data
+
+#---------------------------------
+def error_reply(msg, code = -1):
+    reply = {}
+    reply['status'] = code
+    reply['error'] = msg
+    return reply
