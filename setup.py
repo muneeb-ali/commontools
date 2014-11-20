7ea49d7db6ecf75566270924b2bb5b18022c07a9
Muneeb Ali <muneeb@ali.vc>
2014-11-20 11:44:25 -0500

    upgraded version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
 	name='commontools',
-	version='0.1.0',
+	version='0.1.1',
 	url='https://github.com/muneeb-ali/commontools',
 	license='MIT',
 	author='Muneeb Ali (@muneeb)',
